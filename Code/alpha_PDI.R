52e53e87a8bf7015c8d2862f23075c85bd161c95
Sebastián Montoya-Bustamante <s.montoyabustamante@gmail.com>
2022-09-15 17:10:55 -0300

    New optimization aPDImaxFind2

--- Code/alpha_PDI.R
+++ Code/alpha_PDI.R
@@ -42,6 +42,9 @@ alpha_PDI<-function(data, abun, corrected=T){
     aPDImaxFind <- function(x, q) {
       #For the given number of observations find the maximum possible value of aPDI
       #This function is similar to the one used in the bipartite package (Dormann et al. 2008) to find dmin and calculate d'
+      #expec <- floor(q * (sum(x)-length(x)))
+      #expec<-expec + 1
+      
       expec <- floor(q * (sum(x)))
       restuse <- sum(x) - sum(expec)
       x.new <- expec
@@ -74,10 +77,61 @@ alpha_PDI<-function(data, abun, corrected=T){
       
     }
     
+    aPDImaxFind2 <- function(x, q) {
+      #For the given number of observations find the maximum possible value of aPDI
+      
+      
+        expec <- floor(q * (sum(x)-length(x)))
+        expec<-expec + 1
+        
+        #expec <- floor(q * (sum(x)))
+        restuse <- sum(x) - sum(expec)
+        
+        if (any(expec<0)){
+          restuse<-restuse+sum(expec[which(expec<0)])
+          expec[which(expec<0)]<-0
+        }
+        
+        x.new <- expec
+        i.vec <- 1:length(x)
+        
+        if (restuse!=0){
+          for (j in 1:restuse) {
+            
+            aPDI.check <- numeric(0)
+            xsum <- sum(x.new)
+            
+            p.1 <- x.new/(xsum + 1)/q
+            
+            for (i in i.vec) {
+              pi.1 <- (x.new[i] + 1)/(xsum + 1)/q[i]
+              pen<-c(pi.1, p.1[-i])
+              pen<-pen/sum(pen)
+              sort.p.1 <- sort(pen, decreasing = TRUE)
+              P1.max <- sort.p.1[1]
+              aPDI.check[i] <- 1-sum(P1.max - sort.p.1[-1])/(length(q) - 1)
+            }
+            
+            i.best <- which.max(aPDI.check)[1]
+            x.new[i.best] <- x.new[i.best] + 1
+          }
+          
+        }
+        
+        return(aPDIfun(x.new, q))
+        
+        
+      
+
+    }
+    
     aPDI.raw<-apply(data, 1, aPDIfun, q)
-    aPDI.max<-apply(data, 1, aPDImaxFind, q)
+    aPDI.max1<-apply(data, 1, aPDImaxFind, q)
+    aPDI.max2<-apply(data, 1, aPDImaxFind2, q)
+    aPDI.max<-ifelse(aPDI.max1 > aPDI.max2, aPDI.max1, aPDI.max2)
     aPDI.max<-ifelse(aPDI.raw > aPDI.max, aPDI.raw, aPDI.max)
-    aPDI.unb<-aPDI.raw/aPDI.max
+    aPDI.unb<-ifelse(aPDI.max==0,0, aPDI.raw/aPDI.max)
+    #aPDI.unb<-aPDI.raw/aPDI.max
     return(list(corrected_aPDI=aPDI.unb,raw_aPDI=aPDI.raw, max_aPDI=aPDI.max ))
     
   } else{
