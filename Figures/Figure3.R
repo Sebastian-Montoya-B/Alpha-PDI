262393c5ff1488f930afe37acb0e1098e71769b0
Sebastián Montoya-Bustamante <s.montoyabustamante@gmail.com>
2022-10-25 19:11:48 -0300

    New version of figure 3

--- Figures/Figure3.R
+++ Figures/Figure3.R
@@ -133,59 +133,62 @@ l50<-length(lisEexv)
 
 ######################### 3. PLOTTING ##########################################
 
-
+x11()
 colw<-c("#00ceff", "#078ab5","#004c6d")
-png(filename="Figures/Exported/Figure3.png", width=4000, height=4600, res=600)
+png(filename="Figures/Exported/Figure3.png", width=4600, height=2600, res=600)
 #svg(filename="Figures/Exported/Figure3.svg", width=8, height=7)
 
 
-layout(matrix(c(17,17,1,1,
-                17,17,2,3,
-                4,5,9,10,
-                4,6,11,12,
-                4,7,13,14,
-                4,8,15,16), ncol=4, byrow=T),
-       widths=c(13,7,40,40), heights=c(5,5,rep(90/4,4)))
-layout.show(17)
+layout(t(matrix(c(17,17,1,1,20,
+                17,17,2,3,20,
+                17,17,19,19,20,
+                4,5,9,10,18,
+               4,6,11,12,18,
+                4,7,13,14,18,
+                4,8,15,16,18), ncol=5, byrow=T)),
+       heights=c(10,5,35,35,15), widths=c(5,5,10,rep(80/4,4)))
+
+
+layout.show(20)
 
 #1
 par(mar= c(0,0,0,0),las=1)
 plot(x=NULL, y=NULL, ann=F,xaxt="n", yaxt="n", xlim=c(0,1), ylim=c(0,1), type="n", bty="n")
-text(x=0.5, y=0.55, labels="Resource abundance distribution", cex=1.6)
+text(x=0.5, y=0.5, labels="Resource abundance distribution", cex=1.6,srt=90)
 
 #2
 plot(x=NULL, y=NULL, ann=F,xaxt="n", yaxt="n", xlim=c(0,1), ylim=c(0,1), type="n", bty="n")
-text(x=0.575, y=0.55, labels="Even",  cex=1.6)
+text(x=0.575, y=0.5, labels="Even",  cex=1.4,srt=90)
 
 #3
 plot(x=NULL, y=NULL, ann=F,xaxt="n", yaxt="n", xlim=c(0,1), ylim=c(0,1), type="n", bty="n")
-text(x=0.575, y=0.55, labels="Uneven", cex=1.6)
+text(x=0.575, y=0.5, labels="Uneven", cex=1.4,srt=90)
 
 #4
 plot(x=NULL, y=NULL, ann=F,xaxt="n", yaxt="n", xlim=c(0,1), ylim=c(0,1), type="n", bty="n")
-text(x=0.5, y=0.55, labels="Sampling intensity\n(Number of observations per consumer)",srt=90, cex=1.6)
+text(x=0.5, y=0.55, labels="Sampling intensity\n(Number of observations per consumer)", cex=1.6)
 
 
 #5
 plot(x=NULL, y=NULL, ann=F,xaxt="n", yaxt="n", xlim=c(0,1), ylim=c(0,1), type="n", bty="n")
-text(x=0.5, y=0.7, labels="10",  cex=1.6)
+text(x=0.5, y=0.3, labels="10",  cex=1.4)
 
 #6
 plot(x=NULL, y=NULL, ann=F,xaxt="n", yaxt="n", xlim=c(0,1), ylim=c(0,1), type="n", bty="n")
-text(x=0.5, y=0.7, labels="50",  cex=1.6)
+text(x=0.5, y=0.3, labels="50",  cex=1.4)
 #7
 plot(x=NULL, y=NULL, ann=F,xaxt="n", yaxt="n", xlim=c(0,1), ylim=c(0,1), type="n", bty="n")
-text(x=0.5, y=0.7, labels="80",  cex=1.6)
+text(x=0.5, y=0.3, labels="80",  cex=1.4)
 #8
 plot(x=NULL, y=NULL, ann=F,xaxt="n", yaxt="n", xlim=c(0,1), ylim=c(0,1), type="n", bty="n")
-text(x=0.5, y=0.7, labels="100",  cex=1.6)
+text(x=0.5, y=0.3, labels="100",  cex=1.4)
 
 #9
-par(mar= c(3.4,4,0,1),las=1,cex.lab=1, mgp=c(2.1,0.5,0))
+par(mar= c(1,1,1,1),las=1,cex.lab=1, mgp=c(2.1,0.5,0))
 
 plot(x=lisEexv[1:l5],y=lisEexvs10[1:l5], col=colw[3], pch=21,
      bg=alpha(colw[3],0.5), cex=0.7, 
-     ylab="Observed\ndegree of generalization",
+     ylab="",
      xlab="", xlim=c(0,1), ylim=c(0,1),xaxp=c(0,1,2), yaxp=c(0,1,2),
      yaxt="n", xaxt="n")
 axis(2, at=seq(0,1, length=5), labels=c("0.0","","0.5","","1.0"))
@@ -199,16 +202,17 @@ abline(lm(lisEexv~lisEexvs10), lty=2, lwd=1.5)
 r<-round(cor.test(lisEexv,lisEexvs10)$estimate, digits=2)
 r2<-round(r^2, digits=2)
 md<-round(mean(abs(lisEexv - lisEexvs10)), digits=2)
-text(x=-0.05, y=0.95, labels=bquote(rho == .(r)~~~~~"r"^2 == .(r2)),pos=4, cex=1.2)
+text(x=-0.05, y=0.95, labels=bquote(rho == .(r)~~~"r"^2 == .(r2)),pos=4, cex=1.2)
 text(x=-0.05, y=0.8, labels=bquote("MD" == .(md)), pos=4, cex=1.2)
 
 #10
+
 plot(x=lisUexv2[1:l5],y=lisUexvs2_10[1:l5], col=colw[3], pch=21, 
      bg=alpha(colw[3],0.5), cex=0.7, ylab="", 
      xlab="", xlim=c(0,1), ylim=c(0,1),xaxp=c(0,1,2), yaxp=c(0,1,2),
      yaxt="n", xaxt="n")
-axis(2, at=seq(0,1, length=5), labels=c("","","","",""))
-axis(1, at=seq(0,1, length=5), labels=c("","","","",""))
+axis(2, at=seq(0,1, length=5), labels=c("0.0","","0.5","","1.0"))
+axis(1, at=seq(0,1, length=5), labels=c("0.0","","0.5","","1.0"))
 points(x=lisUexv2[(l5+1):l10],y=lisUexvs2_10[(l5+1):l10],col=colw[2],
        pch=23, bg=alpha(colw[2],0.5),cex=0.7)
 points(x=lisUexv2[(l10+1):l50],y=lisUexvs2_10[(l10+1):l50],col=colw[1],
@@ -218,17 +222,17 @@ abline(lm(lisUexv2~lisUexvs2_10), lty=2, lwd=1.5)
 r<-round(cor.test(lisUexv2,lisUexvs2_10)$estimate, digits=2)
 r2<-round(r^2, digits=2)
 md<-round(mean(abs(lisUexv2-lisUexvs2_10)), digits=2)
-text(x=-0.05, y=0.95, labels=bquote(rho == .(r)~~~~~"r"^2 == .(r2)),pos=4, cex=1.2)
+text(x=-0.05, y=0.95, labels=bquote(rho == .(r)~~~"r"^2 == .(r2)),pos=4, cex=1.2)
 text(x=-0.05, y=0.8, labels=bquote("MD" == .(md)), pos=4, cex=1.2)
 
 #11
 
 plot(x=lisEexv[1:l5],y=lisEexvs50[1:l5], col=colw[3], pch=21,
      bg=alpha(colw[3],0.5), cex=0.7, 
-     ylab="Observed\ndegree of generalization",
+     ylab="",
      xlab="", xlim=c(0,1), ylim=c(0,1),xaxp=c(0,1,2), yaxp=c(0,1,2),
      yaxt="n", xaxt="n")
-axis(2, at=seq(0,1, length=5), labels=c("0.0","","0.5","","1.0"))
+axis(2, at=seq(0,1, length=5), labels=c("","","","",""))
 axis(1, at=seq(0,1, length=5), labels=c("","","","",""))
 points(x=lisEexv[(l5+1):l10],y=lisEexvs50[(l5+1):l10],col=colw[2],
        pch=23, bg=alpha(colw[2],0.5),cex=0.7)
@@ -239,7 +243,7 @@ abline(lm(lisEexv~lisEexvs50), lty=2, lwd=1.5)
 r<-round(cor.test(lisEexv,lisEexvs50)$estimate, digits=2)
 r2<-round(r^2, digits=2)
 md<-round(mean(abs(lisEexv - lisEexvs50)), digits=2)
-text(x=-0.05, y=0.95, labels=bquote(rho == .(r)~~~~~"r"^2 == .(r2)),pos=4, cex=1.2)
+text(x=-0.05, y=0.95, labels=bquote(rho == .(r)~~~"r"^2 == .(r2)),pos=4, cex=1.2)
 text(x=-0.05, y=0.8, labels=bquote("MD" == .(md)), pos=4, cex=1.2)
 
 #12
@@ -249,7 +253,7 @@ plot(x=lisUexv2[1:l5],y=lisUexvs2_50[1:l5], col=colw[3], pch=21,
      xlab="", xlim=c(0,1), ylim=c(0,1),xaxp=c(0,1,2), yaxp=c(0,1,2),
      yaxt="n", xaxt="n")
 axis(2, at=seq(0,1, length=5), labels=c("","","","",""))
-axis(1, at=seq(0,1, length=5), labels=c("","","","",""))
+axis(1, at=seq(0,1, length=5), labels=c("0.0","","0.5","","1.0"))
 points(x=lisUexv2[(l5+1):l10],y=lisUexvs2_50[(l5+1):l10],col=colw[2],
        pch=23, bg=alpha(colw[2],0.5),cex=0.7)
 points(x=lisUexv2[(l10+1):l50],y=lisUexvs2_50[(l10+1):l50],col=colw[1],
@@ -259,17 +263,17 @@ abline(lm(lisUexv2~lisUexvs2_50), lty=2, lwd=1.5)
 r<-round(cor.test(lisUexv2,lisUexvs2_50)$estimate, digits=2)
 r2<-round(r^2, digits=2)
 md<-round(mean(abs(lisUexv2-lisUexvs2_50)), digits=2)
-text(x=-0.05, y=0.95, labels=bquote(rho == .(r)~~~~~"r"^2 == .(r2)),pos=4, cex=1.2)
+text(x=-0.05, y=0.95, labels=bquote(rho == .(r)~~~"r"^2 == .(r2)),pos=4, cex=1.2)
 text(x=-0.05, y=0.8, labels=bquote("MD" == .(md)), pos=4, cex=1.2)
 
 #13
 
 plot(x=lisEexv[1:l5],y=lisEexvs80[1:l5], col=colw[3], pch=21,
      bg=alpha(colw[3],0.5), cex=0.7, 
-     ylab="Observed\ndegree of generalization",
+     ylab="",
      xlab="", xlim=c(0,1), ylim=c(0,1),xaxp=c(0,1,2), yaxp=c(0,1,2),
      yaxt="n", xaxt="n")
-axis(2, at=seq(0,1, length=5), labels=c("0.0","","0.5","","1.0"))
+axis(2, at=seq(0,1, length=5), labels=c("","","","",""))
 axis(1, at=seq(0,1, length=5), labels=c("","","","",""))
 points(x=lisEexv[(l5+1):l10],y=lisEexvs80[(l5+1):l10],col=colw[2],
        pch=23, bg=alpha(colw[2],0.5),cex=0.7)
@@ -280,7 +284,7 @@ abline(lm(lisEexv~lisEexvs80), lty=2, lwd=1.5)
 r<-round(cor.test(lisEexv,lisEexvs80)$estimate, digits=2)
 r2<-round(r^2, digits=2)
 md<-round(mean(abs(lisEexv - lisEexvs80)), digits=2)
-text(x=-0.05, y=0.95, labels=bquote(rho == .(r)~~~~~"r"^2 == .(r2)),pos=4, cex=1.2)
+text(x=-0.05, y=0.95, labels=bquote(rho == .(r)~~~"r"^2 == .(r2)),pos=4, cex=1.2)
 text(x=-0.05, y=0.8, labels=bquote("MD" == .(md)), pos=4, cex=1.2)
 
 #14
@@ -290,7 +294,7 @@ plot(x=lisUexv2[1:l5],y=lisUexvs2_80[1:l5], col=colw[3], pch=21,
      xlab="", xlim=c(0,1), ylim=c(0,1),xaxp=c(0,1,2), yaxp=c(0,1,2),
      yaxt="n", xaxt="n")
 axis(2, at=seq(0,1, length=5), labels=c("","","","",""))
-axis(1, at=seq(0,1, length=5), labels=c("","","","",""))
+axis(1, at=seq(0,1, length=5), labels=c("0.0","","0.5","","1.0"))
 points(x=lisUexv2[(l5+1):l10],y=lisUexvs2_80[(l5+1):l10],col=colw[2],
        pch=23, bg=alpha(colw[2],0.5),cex=0.7)
 points(x=lisUexv2[(l10+1):l50],y=lisUexvs2_80[(l10+1):l50],col=colw[1],
@@ -300,19 +304,19 @@ abline(lm(lisUexv2~lisUexvs2_80), lty=2, lwd=1.5)
 r<-round(cor.test(lisUexv2,lisUexvs2_80)$estimate, digits=2)
 r2<-round(r^2, digits=2)
 md<-round(mean(abs(lisUexv2-lisUexvs2_80)), digits=2)
-text(x=-0.05, y=0.95, labels=bquote(rho == .(r)~~~~~"r"^2 == .(r2)),pos=4, cex=1.2)
+text(x=-0.05, y=0.95, labels=bquote(rho == .(r)~~~"r"^2 == .(r2)),pos=4, cex=1.2)
 text(x=-0.05, y=0.8, labels=bquote("MD" == .(md)), pos=4, cex=1.2)
 
 #15
 
 plot(x=lisEexv[1:l5],y=lisEexvs100[1:l5], col=colw[3], pch=21,
      bg=alpha(colw[3],0.5), cex=0.7, 
-     ylab="Observed\ndegree of generalization",
-     xlab=expression("Expected degree of generalization"),
+     ylab="",
+     xlab="",
      xlim=c(0,1), ylim=c(0,1),xaxp=c(0,1,2), yaxp=c(0,1,2),
      yaxt="n", xaxt="n")
-axis(2, at=seq(0,1, length=5), labels=c("0.0","","0.5","","1.0"))
-axis(1, at=seq(0,1, length=5), labels=c("0.0","","0.5","","1.0"))
+axis(2, at=seq(0,1, length=5), labels=c("","","","",""))
+axis(1, at=seq(0,1, length=5), labels=c("","","","",""))
 points(x=lisEexv[(l5+1):l10],y=lisEexvs100[(l5+1):l10],col=colw[2],
        pch=23, bg=alpha(colw[2],0.5),cex=0.7)
 points(x=lisEexv[(l10+1):l50],y=lisEexvs100[(l10+1):l50],col=colw[1],
@@ -325,14 +329,14 @@ if (r==1){
 }
 r2<-round(r^2, digits=2)
 md<-round(mean(abs(lisEexv - lisEexvs100)), digits=2)
-text(x=-0.05, y=0.95, labels=bquote(rho == .(r)~~~~~"r"^2 == .(r2)),pos=4, cex=1.2)
+text(x=-0.05, y=0.95, labels=bquote(rho == .(r)~~~"r"^2 == .(r2)),pos=4, cex=1.2)
 text(x=-0.05, y=0.8, labels=bquote("MD" == .(md)), pos=4, cex=1.2)
 
 #16
 
 plot(x=lisUexv2[1:l5],y=lisUexvs2_100[1:l5], col=colw[3], pch=21, 
      bg=alpha(colw[3],0.5), cex=0.7, ylab="", 
-     xlab=expression("Expected degree of generalization"), 
+     xlab="", 
      xlim=c(0,1), ylim=c(0,1),xaxp=c(0,1,2), yaxp=c(0,1,2),
      yaxt="n", xaxt="n")
 axis(2, at=seq(0,1, length=5), labels=c("","","","",""))
@@ -346,7 +350,7 @@ abline(lm(lisUexv2~lisUexvs2_100), lty=2, lwd=1.5)
 r<-round(cor.test(lisUexv2,lisUexvs2_100)$estimate, digits=2)
 r2<-round(r^2, digits=2)
 md<-round(mean(abs(lisUexv2-lisUexvs2_100)), digits=2)
-text(x=-0.05, y=0.95, labels=bquote(rho == .(r)~~~~~"r"^2 == .(r2)),pos=4, cex=1.2)
+text(x=-0.05, y=0.95, labels=bquote(rho == .(r)~~~"r"^2 == .(r2)),pos=4, cex=1.2)
 text(x=-0.05, y=0.8, labels=bquote("MD" == .(md)), pos=4, cex=1.2)
 
 #17
@@ -356,5 +360,21 @@ legend(x=0, y=1, legend=c("5", "10", "50"), pch=c(21,23,22),
        pt.bg=c(alpha(colw[3],0.5),alpha(colw[2],0.5),alpha(colw[1],0.5)),
        col=c(colw[3],colw[2],colw[1]), title="Potential resources",
        horiz=T, pt.cex=2, cex=1.3, bty="n")
+
+
+
+#18
+
+
+par(mar= c(0,0,0,0),las=1)
+plot(x=NULL, y=NULL, ann=F,xaxt="n", yaxt="n", xlim=c(0,1), ylim=c(0,1), type="n", bty="n")
+text(x=0.5, y=0.5, labels="Expected degree of generalization", cex=1.3)
+
+#19
+
+par(mar= c(0,0,0,0),las=1)
+plot(x=NULL, y=NULL, ann=F,xaxt="n", yaxt="n", xlim=c(0,1), ylim=c(0,1), type="n", bty="n")
+text(x=0.5, y=0.5, labels="Observed degree of generalization", cex=1.3,srt=90)
+
 
 dev.off()
